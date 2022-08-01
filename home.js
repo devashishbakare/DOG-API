a7e2944d2bf28057bc3321e2ed0f1c86ce901ad4
devashish bakare <devashishbakare@gmail.com>
2022-08-01 17:00:03 +0530

    Added Css using Jquery

---
+++ home.js
@@ -0,0 +1,31 @@
+$("body").css({
+    "display" : "flex",
+    "flex-direction" : "column",   
+    "justify-content" : "center",
+    "align-items" : "center",
+    "margin" : "100px"
+
+});
+$("#div-button").height("50px");
+$("#div-button").width("auto");
+
+$("#button").css({
+    "height" : "45px",
+    "width" : "auto",
+    "color" : "white",
+    "background-color" : "black",
+    "font-size" : "1rem",
+    "font-family" : "cursive",
+    "box-shadow" : "5px 5px 5px black"
+});
+
+$("#image-div").css({
+    "border" : "2px solid black",
+    "display" : "block",
+    "margin-top" : "15px"
+})
+
+$("dogImage").css({
+    "height" : "auto",
+    "width" : "auto"
+});
